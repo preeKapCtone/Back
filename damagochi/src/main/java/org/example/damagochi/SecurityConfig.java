85d8c6e0fcc86a040fbea438918fb79d1b32b732
22-JWL <jck1004ee@gmail.com>
2024-11-22 14:46:58 +0900

    add wrapper properties

--- damagochi/src/main/java/org/example/damagochi/SecurityConfig.java
+++ damagochi/src/main/java/org/example/damagochi/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 비활성화
                 .authorizeHttpRequests(auth -> auth
 
-                        .requestMatchers("/api/signup", "/api/login").permitAll() // 인증 없이 접근 가능한 경로
+                        .requestMatchers("/api/signup", "/api/login", "/*").permitAll() // 인증 없이 접근 가능한 경로
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
 
                 )
